8f0d06d3cf3fabbe3b8cebe0b25a7fc497879782
Andreas Auernhammer <github@aead.dev>
2024-10-14 16:53:24 +0200

    add support for boringcrypto FIPS build mode
    This commit adds the `internal/boringcrypto` package
    that exposes a single constant `FIPS=true` if the package
    is built with the build tag `boringcrypto` and `FIPS=false`
    otherwise.
    
    The Go standard library (unofficially) supports a FIPS module
    via the `GOEXPERIMENT=boringcrypto` env. variable. If set, the
    Go compiler includes all source files with the build tag
    ```
    go:build boringcrypto
    ```
    
    In this "FIPS mode", we don't support Ed25519 since it's a cryptographic
    primitive that cannot be used as per the FIPS 140-2 standard. EDDSA over
    the curves P-256, P-384 and P-521 is still supported.
    
    This needs to be re-visited once golang/go#69536 lands.
    
    Signed-off-by: Andreas Auernhammer <github@aead.dev>

---
+++ internal/boring/boring.go
@@ -0,0 +1,9 @@
+// Copyright (c) 2024 Andreas Auernhammer. All rights reserved.
+// Use of this source code is governed by a license that can be
+// found in the LICENSE file.
+
+//go:build boringcrypto
+
+package boring
+
+const FIPS = true
