9b9cfcb64687e66270ea6908b84af55d13a466f9
Andreas Auernhammer <github@aead.dev>
2025-06-02 20:45:50 +0200

    remove boringcrypto in favour of the new `fips140` module
    With Go 1.24, the standard library includes a FIPS 140-3 module
    that is currently under certification. Go programs can be started
    with FIPS 140-3 compliant crypto implementations via `GODEBUG=fips140=on`.
    
    Hence, the experimental and not officially supported boringcrypto
    checks can be dropped. For additional information around Go and FIPS 140-3
    refer to: https://go.dev/doc/security/fips140
    
    Signed-off-by: Andreas Auernhammer <github@aead.dev>

--- key_test.go
+++ key_test.go
@@ -15,15 +15,11 @@ import (
 	"testing"
 
 	"aead.dev/mtls"
-	"aead.dev/mtls/internal/boring"
 )
 
 // TestGenerateKeyEdDSA tests whether generated EdDSA private keys
 // are equal to their parsed textual representation
 func TestGenerateKeyEdDSA(t *testing.T) {
-	if boring.FIPS {
-		t.Skip("Ed25519 is not available in FIPS mode")
-	}
 	t.Parallel()
 
 	key, err := mtls.GenerateKeyEdDSA(rand.Reader)
@@ -73,10 +69,6 @@ func TestGenerateKeyECDSA(t *testing.T) {
 // identity of the corresponding private key.
 func TestPrivateKey_Identity(t *testing.T) {
 	for _, test := range privateKeyIdentityTests {
-		if boring.FIPS && test.SkipFIPS {
-			continue
-		}
-
 		key, err := mtls.ParsePrivateKey(test.PrivateKey)
 		if err != nil {
 			t.Fatalf("failed to parse private key %s: %v", test.PrivateKey, err)
@@ -107,12 +99,10 @@ func TestPrivateKey_Identity(t *testing.T) {
 var privateKeyIdentityTests = []struct {
 	Filename   string
 	PrivateKey string
-	SkipFIPS   bool // Skip test in FIPS mode
 }{
 	{
 		Filename:   "./testdata/certs/ed25519.crt",
 		PrivateKey: "k1:xZnpcYtPdVMNLBBRaUO5HPEoK_jVrcc3MWR8BshkjJw",
-		SkipFIPS:   boring.FIPS,
 	},
 	{
 		Filename:   "./testdata/certs/p-256.crt",
